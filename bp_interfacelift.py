bc0f73248775c35a74b409a7893e096bfff95cbe
Bruno Paulin <brunopaulin@bpaulin.net>
2010-05-27 12:08:42 +0200

    Création du projet

--- bp_interfacelift.py
+++ bp_interfacelift.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 #       bp_interfacelift.py
 #       
-#       Copyright 2010 Bruno Paulin <bruno@Fernand>
+#       Copyright 2010 Bruno Paulin <brunopaulin@bpaulin.net>
 #       
 #       This program is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
